0481ba199ffddebea9550768e818edc91791f0bc
omerkulahci99 <omerkulahci99@gmail.com>
2023-06-06 23:21:51 +0300

    PSVM SOUT

---
+++ src/main/java/com/omerkulahci/lesson1/PsvmTutorials.java
@@ -0,0 +1,13 @@
+package com.omerkulahci.lesson1;
+
+// sadece 1 tane public class vardır (inner class hariç)
+// static class yoktur. (inner class hariç)
+public class PsvmTutorials {
+    // psvm => TAB
+    public static void main(String[] args) {
+
+        //sout => TAB
+        System.out.println("");
+
+    }
+}
