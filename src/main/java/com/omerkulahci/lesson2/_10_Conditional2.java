3aefe4f3c5c4267298f2d3367af3739142dfaa5e
omerkulahci99 <omerkulahci99@gmail.com>
2023-06-18 21:54:43 +0300

    conditional

--- src/main/java/com/omerkulahci/lesson2/_10_Conditional2.java
+++ src/main/java/com/omerkulahci/lesson2/_10_Conditional2.java
@@ -4,18 +4,44 @@ public class _10_Conditional2 {
 
     public static void main(String[] args) {
 
-        Scanner klavye=new Scanner(System.in);
-        System.out.println("Lütfen sayı giriniz");
-        int number=klavye.nextInt();
-        if(number>0){
-            System.out.println("Pozitif");
-        }else
-            System.out.println("negatif");
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Lütfen hafta içi gün sayısını giriniz");
+        int number = klavye.nextInt();
 
-        String ternary=(number>0) ? "Pozitif":"negatif";
-        System.out.println(ternary);
+        // if else if
+        if (number == 1) {
+            System.out.println("pazartesi");
+        } else if (number == 2) {
+            System.out.println("salı");
+        } else if (number == 3)
+            System.out.println("çarşamba");
+        else if (number == 4)
+            System.out.println("perşembe");
+        else if (number == 5)
+            System.out.println("cuma");
+        else
+            System.out.println("farklı gün");
 
-
-
+        //switch
+        switch (number) {
+            case 1:
+                System.out.println("pazartesi");
+                break;
+            case 2:
+                System.out.println("salı");
+                break;
+            case 3:
+                System.out.println("çarşamba");
+                break;
+            case 4:
+                System.out.println("perşembe");
+                break;
+            case 5:
+                System.out.println("cuma");
+                break;
+            default:
+                System.out.println("farklı gün");
+                break;
+        }
     }
 }
